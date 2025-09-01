60b60527d38346d5e9a9b942cfbc4c2d939492bb
Cristina <hainberg@technikum-wien.at>
2025-09-01 15:49:46 +0200

    Added methods getUniqueCode and exists to LvevaluierungCode_model..php
    . getUniqueCode: Returns a unique Code.
    . exists: Checks if code already exists.

--- models/LvevaluierungCode_model.php
+++ models/LvevaluierungCode_model.php
@@ -8,4 +8,48 @@ class LvevaluierungCode_model extends DB_Model
 		$this->dbTable = 'extension.tbl_lvevaluierung_code';
 		$this->pk = 'lvevaluierung_code_id';
 	}
+
+	/**
+	 * Returns a unique Code.
+	 *
+	 * @return code
+	 */
+	public function getUniqueCode()
+	{
+		$found = false;
+
+		while(!$found)
+		{
+			$possibleChars = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+			$code = '';
+
+			for($i = 0; $i < 11; $i++)
+			{
+				$rand = rand(0, strlen($possibleChars) - 1);
+				$code .= substr($possibleChars, $rand, 1);
+			}
+
+			if ($this->exists($code) === false)
+			{
+				$found = true;
+			}
+		}
+
+		return $code;
+	}
+
+	/**
+	 * Checks if code already exists.
+	 *
+	 * @param $code Code
+	 * @return code|false False if not found, Code if found.
+	 */
+	public function exists($code)
+	{
+		$result = $this->loadWhere([
+			'code' => $code
+		]);
+
+		return hasData($result) ? getData($result)[0] : false;
+	}
 }
