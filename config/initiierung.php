f8cad4e1ef45671d7029e5e8dec3c6d102727aab
Cristina <hainberg@technikum-wien.at>
2025-09-17 14:53:18 +0200

    Added config 'lvLeitungRequired'

--- config/initiierung.php
+++ config/initiierung.php
@@ -4,3 +4,6 @@ $config = [];
 
 // Define Lehrformen, that should not be evaluated
 $config['excludedLehrformen'] = ['EXAM', 'BE', 'FL'];
+
+// Define if LV-Leitung is required for initializing Lvevaluierung
+$config['lvLeitungRequired'] = true;
