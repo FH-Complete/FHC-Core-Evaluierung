51598c6ac5ed268589e62e3d59622e80804cfe85
Cristina <hainberg@technikum-wien.at>
2025-09-24 13:23:22 +0200

    Added config filterLehreinheitenByUniqueLectorAndGruppen
    Defines filter to get only Lehreinheiten with unique 'Gruppenzusammensetzung'+Lector

--- config/initiierung.php
+++ config/initiierung.php
@@ -7,3 +7,6 @@ $config['excludedLehrformen'] = ['EXAM', 'BE', 'FL'];
 
 // Define if LV-Leitung is required for initializing Lvevaluierung
 $config['lvLeitungRequired'] = true;
+
+// Define filter to get only Lehreinheiten with unique 'Gruppenzusammensetzung'+Lector
+$config['filterLehreinheitenByUniqueLectorAndGruppen'] = true;
