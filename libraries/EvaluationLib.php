486019f538cb96fd4a1d52dc3db586b59f22bf0e
Cristina <hainberg@technikum-wien.at>
2026-08-04 11:33:33 +0200

    Fixed: präzisere HLE-Werte bei kleinen Gruppen-Evaluierungen (gerades Gesamtgewicht)
    Bisher nur unterer Wert statt Mittel der zwei mittleren Positionen

--- libraries/EvaluationLib.php
+++ libraries/EvaluationLib.php
@@ -356,17 +356,25 @@ class EvaluationLib
 
 		// Weighted median
 		$totalWeight = array_sum(array_column($pairs, 'weight'));
-		$medianPos   = $totalWeight / 2;
+
+		// beide mittleren Positionen ermitteln
+		$lowerPos = ceil($totalWeight / 2);
+		$upperPos = ceil(($totalWeight + 1) / 2);
 
 		$cumWeight = 0;
+		$lowerVal = null;
+		$upperVal = null;
 		foreach ($pairs as $pair) {
 			$cumWeight += $pair['weight'];
-			if ($cumWeight >= $medianPos) {
-				return round($pair['value'], 2);
+			if ($lowerVal === null && $cumWeight >= $lowerPos) $lowerVal = $pair['value'];
+			if ($cumWeight >= $upperPos)
+			{
+				$upperVal = $pair['value'];
+				break;
 			}
 		}
 
-		return null;
+		return round(($lowerVal + $upperVal) / 2, 1);;
 	}
 
 
